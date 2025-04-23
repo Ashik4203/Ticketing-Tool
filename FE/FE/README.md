acf4a9721c0d6e99a68f59901a9b29412fe90c9c
Ashik4203 <mohdashik4203@gmail.com>
2025-04-23 14:08:07 +0530

    Cleaned up folder structure and removed duplicate folders

--- FE/FE/README.md
+++
@@ -1,1 +0,0 @@
-# TicketingTool-FrontEnd
--- FE/NEW-BE/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('ticketingtool', 'root', '12345', {
-  host: 'localhost',
-  dialect: 'mysql', // or 'postgres', 'sqlite', etc.
-  logging: false,   // disable logging if you want
-});
-
-module.exports = sequelize;
